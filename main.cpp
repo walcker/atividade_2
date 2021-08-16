34619ba94839bbd8304dc5ddbfe0013efb549bdc
Walcker Gomes <53241419+walcker@users.noreply.github.com>
2021-08-16 15:16:07 +0000

    Pronto

--- main.cpp
+++ main.cpp
@@ -13,17 +13,17 @@
 
 using namespace std;
 
-int busca_binaria(int vetor[8], int valorProcurado, int *posicaoEncontrada){
+int busca_binaria(int vetor[9], int valorProcurado, int *posicaoEncontrada){
 
     int esquerda = 0;       //Limite da esquerda
-    int direita = 7;  //Limite da direita
-    int meio = 4;               //Meio onde fica o cursor
+    int direita = 8;  //Limite da direita
+    int meio = (esquerda + direita)/2;               //Meio onde fica o cursor
 
     while(vetor[meio] != valorProcurado){
         //Quando o valor do meio é encontrado
         if(valorProcurado == vetor[meio]){
             *posicaoEncontrada = meio;
-            return 1;
+            return meio;
         //Ajustando o nomo meio
         }else if(vetor[meio] < valorProcurado){
             meio = (meio + direita)/2;
@@ -68,9 +68,43 @@ int main(){
   //Busca binária
   valorProcurado = 30;
   if(busca_binaria(vetor, valorProcurado, &posicaoEncontrada) == 1){
-        cout << "O valor foi encontrado na posição: " << posicaoEncontrada << endl;
+        cout << "O valor 30 foi encontrado na iteração: " << posicao << endl;
     }else{
         cout << "O valor não foi encontrado." << endl;
+    }
+
+    //Busca sequencial crescente
+    aux = 0;
+  for(int i = 0; i < 9; i++){
+    if(vetor[i] == 13){
+      cout << "O número 30 pertence ao vetor na procura sequencial crescente";
+    }else{
+      aux++;
+    }
+    if(aux == 9){
+      cout << "O número 13 não foi encontrado." << endl;
+    }
+  }
+
+  //Busca sequencial decrescente
+  aux = 0;
+  for(int i = 9; i > 0; i--){
+    if(vetor[i] == 13){
+      cout << "O número 30 pertence ao vetor na procura sequencial decrescente";
+    }else{
+      aux++;
+    }
+    if(aux == 9){
+      cout << "O número 13 não foi encontrado." << endl;
+    }
+  }
+
+  //Busca binária
+  valorProcurado = 13;
+  if(busca_binaria(vetor, valorProcurado, &posicaoEncontrada) == -1){
+        cout << "O valor 13 foi encontrado na iteração: " << posicao << endl;
+    }else{
+        cout << "O valor 13 não foi encontrado." << endl;
     }
 
     return 0;
